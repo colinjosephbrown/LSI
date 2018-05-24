c728b6c37b86673dc9bd08b6d9bd2a9e542182d8
colinjosephbrown <39393709+colinjosephbrown@users.noreply.github.com>
2018-05-24 12:00:23 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='LSI',
+      version='0.1',
+      description='Local synthetic instances (LSI)',
+      url='http://github.com/colinjosephbrown/LSI',
+      author='Colin Joseph Brown',
+      author_email='colinjosephbrown@gmail.com',
+      license='',
+      packages=['LSI'],
+      zip_safe=False)
