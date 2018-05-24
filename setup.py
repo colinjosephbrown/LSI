0f23d7e11b2cc63c4ab654588c7935d6bba4c501
Colin Brown <dongwook.cho@wrnch.com>
2018-05-24 17:17:41 -0400

    Packaging it all up

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
+import os.path
 from setuptools import setup
 
-setup(name='LSI',
+LOCAL_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Get requirements
+requirements = []
+with open(os.path.join(LOCAL_DIR, 'requirements.txt'), 'r') as infile:
+    for line in infile:
+        line = line.strip()
+        if line and not line[0] == '#':
+            requirements.append(line)
+
+setup(name='lsi-oversampling',
       version='0.1',
       description='Local synthetic instances (LSI)',
       url='http://github.com/colinjosephbrown/LSI',
       author='Colin Joseph Brown',
       author_email='colinjosephbrown@gmail.com',
-      license='',
-      packages=['LSI'],
-      zip_safe=False)
+      license='Pending',
+      packages=['lsi-oversampling'],
+      install_requires=requirements)
